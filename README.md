571991582cd4c45614fef45441227c52ea6189e9
chris <chris@coronis>
2014-02-10 01:53:29 -0500

    Added migrations.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ django-federal-reserve - Django models for storing Federal Reserve data
 Overview
 ========
 
+Usage
+=====
+
     python manage.py data_mirror_refresh FederalReserveDataSource
     
     python manage.py data_mirror_refresh FederalReserveDataSource --ids=WUPSHO
---
+++ django_federal_reserve/migrations/0005_auto__add_field_series_date_is_start.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Series.date_is_start'
+        db.add_column(u'django_federal_reserve_series', 'date_is_start',
+                      self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Series.date_is_start'
+        db.delete_column(u'django_federal_reserve_series', 'date_is_start')
+
+
+    models = {
+        'django_federal_reserve.data': {
+            'Meta': {'ordering': "('series', '-date')", 'unique_together': "(('series', 'date'),)", 'object_name': 'Data'},
+            'date': ('django.db.models.fields.DateField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'series': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'data'", 'to': "orm['django_federal_reserve.Series']"}),
+            'value': ('django.db.models.fields.FloatField', [], {})
+        },
+        'django_federal_reserve.series': {
+            'Meta': {'object_name': 'Series'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
+            'date_is_start': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            'frequency': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '150', 'primary_key': 'True', 'db_index': 'True'}),
+            'last_updated': ('django.db.models.fields.DateField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'max_date': ('django.db.models.fields.DateField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'min_date': ('django.db.models.fields.DateField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'popularity': ('django.db.models.fields.IntegerField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'seasonal_adjustment': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'units': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['django_federal_reserve']
